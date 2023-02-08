fd70961c1bf0898e2de3905563bf4694078a0cf1
Humera Naz <humeranaz911@gmail.com>
2023-02-09 04:16:45 +0530

    checking

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-import pymongo
+from Insurance.components import data_ingestion,data_validation,data_transformation,model_trainer,model_evaluation, model_pusher
+from Insurance.logger import logging
+from Insurance.exception import InsuranceException
+from Insurance.utils import get_collection_as_dataframe
+import sys, os
+from Insurance.entity import config_entity, artifact_entity
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27020/neurolabDB")
+if __name__ == "__main__":
+     try:
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = data_ingestion.DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion()) 
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation = data_validation.DataValidation(data_validation_config=data_validation_config,
+                    data_ingestion_artifact=data_ingestion_artifact)
+          data_validation_artifact = data_validation.initiate_data_validation()
+          data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+          data_transformation = data_transformation.DataTransformation(data_transformation_config=data_transformation_config, 
+          data_ingestion_artifact=data_ingestion_artifact)
+          data_transformation_artifact = data_transformation.initiate_data_transformation()
+          model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+          model_trainer = model_trainer.ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+          model_trainer_artifact = model_trainer.initiate_model_trainer()
+          
+          model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+          model_eval = model_evaluation.ModelEvaluation(model_eval_config=model_eval_config, 
+                                                       data_ingestion_artifact=data_ingestion_artifact, 
+                                                       data_transformation_artifact=data_transformation_artifact, 
+                                                       model_trainer_artifact=model_trainer_artifact)
+          model_evaluation_artifact = model_eval.initiate_model_evaluation()     
 
-# Database Name
-dataBase = client["neurolabDB"]
-
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+          model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
+          model_pusher = model_pusher.ModelPusher(model_pusher_config=model_pusher_config, 
+                                                  data_transformation_artifact=data_transformation_artifact, 
+                                                  model_trainer_artifact=model_trainer_artifact)
+          model_pusher_artifact = model_pusher.initiate_model_pusher()             
+     except Exception as e:
+          raise InsuranceException(e,sys)
